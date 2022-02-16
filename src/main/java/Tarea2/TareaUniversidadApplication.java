65e25c160bcb8268f2cf88486a625d59ed6434e2
gpacheco <gpacheco@CS-INF-07.coopemail.coopeservidores.fi.cr>
2022-02-16 00:43:12 -0600

    TAREA#2

---
+++ src/main/java/Tarea2/TareaUniversidadApplication.java
@@ -0,0 +1,13 @@
+package Tarea2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaUniversidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaUniversidadApplication.class, args);
+	}
+
+}
